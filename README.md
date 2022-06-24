908cbfef1abecc2584f64a0f0a52c42bf34c5df6
itaminul@gmail.com <itaminul@gmail.com>
2022-06-24 23:35:13 +0600

    create route middleware

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 npm init
 npm i express
+
+6->express->15 end ->start 16
--- app.js
+++ app.js
@@ -1,10 +1,111 @@
+const fs = require('fs')
 const express = require('express')
 const app = express();
+app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.status(200)
-    .json({ message: 'Hello form the server', app:'nature'});
+app.use((req,res, next) => {
+    console.log('hello middleware')
+    next();
 })
+
+app.use((req,res, next) => {
+    req.reqestTime = new Date().toISOString;
+    next();
+})
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`))
+
+const getAllTours = (req,res) => {
+    console.log(req.reqestTime);
+    res.status(200).json({
+        status: 'success',
+        requestedAtt: req.reqestTime,
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+
+}
+const createTour = (req,res) => {
+console.log('createTour')
+}
+const getTour = (req,res) => {
+    console.log('createTour')
+    }
+    
+const updateTour = (req,res) => {
+    console.log('createTour')
+    }
+const deleteTour = (req,res) => {
+    console.log('createTour')
+    }
+            
+        
+
+// app.post('/api/v1/tours',getTours)
+// app.post('/api/v1/tours/:id',updateTours)
+// app.post('/api/v1/tours/:id', deleteTours)
+
+app
+.route('/api/v1/tours')
+.get(getAllTours)
+.post(createTour);
+
+app
+.route('/api/v1/tours/:id')
+.get(getTour)
+.patch(updateTour)
+.delete(deleteTour);
+
+
+const getAllUsers = (req,res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yes default'
+    })
+}
+
+const createUser = (req,res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yes default'
+    })
+}
+
+const getUser = (req,res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yes default'
+    })
+}
+const updateUser = (req,res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yes default'
+    })
+}
+
+const deleteUser = (req,res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yes default'
+    })
+}
+
+app
+.route('/api/v1/users')
+.get(getAllUsers)
+.post(createUser)
+
+app
+.route('/api/v1/users/:id')
+.get(getUser)
+.patch(updateUser)
+.delete(deleteUser)
+
+
+
 const port = 8002;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
