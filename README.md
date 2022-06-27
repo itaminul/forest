c4768fbc8b4be365d06fdd2eb61bf693382ba2a2
itaminul@gmail.com <itaminul@gmail.com>
2022-06-27 21:27:04 +0600

    modification route with controller

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ npm init
 npm i express
 npm install -g nodemon
 
+6->express->17 end ->start 18
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,17 @@
 const fs = require('fs')
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/tours-simple.json`))
 
+exports.checkId = (req, res, next, val) => {
+    console.log(`Tour id is : ${val}`);
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req,res) => {
     //  console.log('req.reqestTime');
     res.status(200).json({
@@ -17,12 +28,30 @@ exports.createTour = (req,res) => {
 console.log('createTour')
 }
 exports.getTour = (req,res) => {
-    console.log('createTour')
+    res.status(200).json({
+        status: 'success',
+        requestedAtt: req.reqestTime,
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
     }
     
     exports.updateTour = (req,res) => {
-    console.log('createTour')
+        res.status(200).json({
+            status: 'success',
+            requestedAtt: req.reqestTime,
+            data: {
+                tours: '<Updated tour here...>'
+            }
+        })
     }
     exports.deleteTour = (req,res) => {
-    console.log('createTour')
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Update tour here...>'
+        }
+    })
     }   
